8754d99cd152c9bb1b50f82d8d269a62eae4365e
Pedro Barreto <pbdeoliveira@gmail.com>
2021-07-08 16:27:18 -0300

    Requisito 1 ao 5

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //inicio do script
+
+//função criar box de cores 
+
+
+
+
+//função criar quadrados de pixel
+let pixelLine = document.querySelectorAll('.pixelLine');
+for (i=0; i < 5; i += 1) {
+  pixelLineItens = pixelLine[i]
+  for (let index = 0; index < 5; index += 1) {
+    let pixels = document.createElement('div');;
+  pixels.className = 'pixel';
+  pixelLineItens.appendChild(pixels);
+  }
+}
+
