5fbe45060cf202d3ea80ae728dda38e7319cd1b8
Pedro Barreto <pbdeoliveira@gmail.com>
2021-07-09 13:35:35 -0300

    Requisitos 8 e 9

--- script.js
+++ script.js
@@ -6,36 +6,42 @@ window.onload = function () {
   mainColor.classList.add('selected');
 }
 
-
-
 //função criar quadrados de pixel
-let pixelLine = document.querySelectorAll('.pixelLine');
+let pixelRow = document.querySelectorAll('#pixelRow');
 for (i=0; i < 5; i += 1) {
-  pixelLineItens = pixelLine[i]
+  pixelRowItens = pixelRow[i]
   for (let index = 0; index < 5; index += 1) {
     let pixels = document.createElement('div');;
   pixels.className = 'pixel';
-  pixelLineItens.appendChild(pixels);
+  pixelRowItens.appendChild(pixels);
   }
 }
 
 //função de selecionar cores 
 function colorPicker() {
-  let colorPalette = document.getElementById('color-palette');
-  let black = 'black';
   let colorSelected = document.getElementsByClassName('selected');
-  colorPalette.addEventListener('click', function(event) {
+  addEventListener('click', function(event) {
+    let alvo = event.target
     for(let i = 0; i < colorSelected.length; i += 1) {
-        colorSelected[i].classList.remove('selected');  
-      }
-      let alvo = event.target
-      alvo.classList.add('selected');
-      black = alvo.style.backgroundColor
+      if( alvo.className === 'color') { 
+      colorSelected[i].classList.remove('selected');
+      alvo.classList.add('selected');   
+      } else if ( alvo.className === 'pixel') {
+      alvo.style.backgroundColor = colorSelected[0].id;
+      }}
       })
     };
   
 colorPicker();
 
-
-
-
+//função do botão limpar
+function clearButton() {
+  let clear = document.getElementById('clear-board');
+  let pixelSelect = document.getElementsByClassName('pixel');
+  clear.addEventListener('click', function(event) {
+    for(let i =0; i < pixelSelect.length; i += 1) {
+    pixelSelect[i].style.backgroundColor = '#fff'  
+    }
+      })
+    };
+clearButton();
